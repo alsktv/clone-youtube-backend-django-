cc4c08fe32fb8bc52c904d53aa768a4ca0d18ed1
alsktv <alsktv@naver.com>
2024-06-10 16:19:55 +0900

    cheakpoint1

--- users/serializers.py
+++ users/serializers.py
@@ -36,6 +36,7 @@ class UserDetailSerialzer(ModelSerializer):
   alert = AlertVideoSerializer(many = True)
   subscribe = TinyUserSerializer(read_only = True ,  many = True) #read_only는 유저가 입력하는 것을 무시하고 내가 값을 직접 입력하겠다는 뜻. 값을 커스텀 해서 집어넣어야 할때도 사용해야함.
 
+
   recent_video1 = videoSerializer(read_only = True)
   recent_video2 = videoSerializer(read_only = True)
   recent_video3 = videoSerializer(read_only = True)
@@ -62,6 +63,7 @@ class UserDetailSerialzer(ModelSerializer):
         "recent_video3",
         "recent_video4",
         "recent_video5",
+        "videos",
       ]
 
 
